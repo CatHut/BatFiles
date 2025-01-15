8034cc79b50d6649b6db17f5f05ab0f5393c2471
Cathut <necota2496@gmail.com>
2025-01-15 21:34:00 +0900

    スペースを含むパスに対応

--- ffmpeg/extact_audio/extact_audio.bat
+++ ffmpeg/extact_audio/extact_audio.bat
@@ -1,12 +1,15 @@
+@echo off
 chcp 65001
 
-@echo off
 rem ドラッグ＆ドロップされた動画ファイルのパスを変数に代入
-set input=%1
+set "input=%~1"
+
 rem 動画ファイルの拡張子を除いたファイル名を変数に代入
-set output=%~n1
+set "output=%~n1"
+
 rem ffmpegコマンドで音声ファイルを抽出
 ffmpeg -i "%input%" -vn "%output%.wav"
+
 rem 終了メッセージを表示
 echo Done!
-
+pause
